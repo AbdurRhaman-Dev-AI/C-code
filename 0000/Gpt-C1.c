ce917014467288d49350f90abfc60b1200cfc981
693WeeB <raferafe980@gmail.com>
2024-11-05 00:34:35 +0600

    first commit

---
+++ 0000/Gpt-C1.c
@@ -0,0 +1,117 @@
+// // #include<stdio.h>
+
+// // int main(){
+// //     printf("Hello,world!\n");
+
+// // return 0;
+
+// // }
+
+// // #include<stdio.h>
+// // int main (){
+// //     printf("nn\n\nnn\n");
+// //     printf("nn/n/nnn/n");
+
+// // // what i predict the ans:
+
+// // // nn
+
+// // // nn
+// // // nn/n/n/nnn/n
+
+ 
+// //     return 0;
+// // }
+
+
+
+
+
+
+// // #include <stdio.h>
+
+// // int main() {
+// //     int x = 10;
+// //     int y = 3;
+    
+// //     x = x + 2 * y;  // Line 1
+// //     y = x - y;      // Line 2
+// //     x = x / y;      // Line 3
+    
+// //     // printf("x = %d, y = %d\n", x, y);
+// //     printf("x = %d, y = %d", x, y);
+    
+// //     return 0;
+// // }
+
+
+
+// // #include <stdio.h>
+
+// // int main() {
+// //     int i, Z = 0;
+
+// //     for (i = 1; i <= 4; i++) {
+// //         Z = Z + i;
+// //     }
+
+// //     printf(" %d", Z );
+    
+// //     return 0;
+// }
+
+// // #include<stdio.h>
+
+// // int main(){
+
+// //     int i, Z =0;
+
+// //     for (i = 1; i <= 4; i++)
+// //     {
+// //         Z = Z + i;
+// //     }
+
+// //     printf("%d",Z);
+
+    
+// // }
+
+
+// #include <stdio.h>
+
+// int main() {
+//     int i, sum = 0;  // Step 1: Initialize variables
+
+//     for (i = 1; i <= 4; i++) {  // Step 2: Start loop with i from 1 to 4
+//         sum = sum + i;          // Step 3: Update sum
+//     }
+
+//     printf("Sum = %d\n", sum); // Step 4: Output the value of sum
+    
+//     return 0; // End of the program
+// }
+
+
+
+
+
+
+
+#include<stdio.h>
+
+int main(){
+
+    float x;
+    scanf("%f",&x);
+
+    int y;
+    y = x;
+
+   
+
+    float Z;
+    Z = x - y;
+    // float Z = x - y ;
+
+    printf("%f",Z);
+}
